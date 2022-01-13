19815098ea28d745000b9190c7a794302ee8263d
SuryamoorthyK <96655710+SuryamoorthyK@users.noreply.github.com>
2022-01-13 10:26:54 +0530

    Fixed code changes

--- week1/assignments/Palindrome.java
+++ week1/assignments/Palindrome.java
@@ -5,25 +5,19 @@ public class Palindrome {
 	public static void main(String[] args) {
 		
 		int num = 34343;
-		int rem = 0 ;
+		int rev = 0 ;
 		int temp = num ;
-		while(num>0){
-			int rev = num%10 ;
-		        rem = rem*10 + rev ;
+		while(num!=0){
+			int rem = num%10 ;
+		        rev = rev*10 + rem ;
 			num = num / 10 ;
 			
 		}
-		if (temp==num) {
+		if (temp==rev) {
 			System.out.println("palindrome");
 		}
 		else {
 			System.out.println("Not palindrome");
 		}
 	}
-			
-		
-
-	
-
 }
-
